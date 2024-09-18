842bac0c2e6d22d85adeb7aa3ced5353bea6929a
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-09-17 21:56:00 -0300

    initial configuration

---
+++ commands.sh
@@ -0,0 +1,7 @@
+npm init -y
+npm i express sequelize pg pg-hstore
+npm i dotenv
+npm i --save-dev typescript @types/node @types/express @types/sequelize ts-node
+npm i --save-dev nodemon @types/bcryptjs @types/jsonwebtoken @types/body-parser
+npm i --save-dev sequelize-cli
+npx tsc --init
