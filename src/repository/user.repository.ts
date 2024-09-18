0eebe27d23693a919b22b2f6b509628d0e7d43a9
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-09-18 02:55:05 -0300

    Create user interface repository

---
+++ src/repository/user.repository.ts
@@ -0,0 +1,10 @@
+import User from "../models/user.model";
+import { UserAttributes, UserCreationAttributes } from "../interface/user.interface";
+
+export interface IUserRepository {
+    createUser(user: UserCreationAttributes): Promise<User>;
+    findAllUsers(): Promise<User[]>;
+    findByIdUser(id: number): Promise<User | null>;
+    updateUser(id: number, user: Partial<UserAttributes>): Promise<User | null>;
+    deleteUser(id: number): Promise<void>;
+}
