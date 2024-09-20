b4716924ac89eb65369812df1699c2b976fe0ba1
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-09-19 22:01:38 -0300

    adding Swagger and user route

--- src/web/app.ts
+++ src/web/app.ts
@@ -1,7 +1,17 @@
 import express from 'express';
+import userRouter from '../routes/user.router';
+
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from '../swagger/swagger.json';
+
 
 const app = express();
-
 app.use(express.json());
+
+// Configurando o Swagger
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+//Routes
+app.use("/", userRouter)
 
 export default app;
