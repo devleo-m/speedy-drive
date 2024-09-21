79d4a70bff29fdf7582d133fb0b8dc033ed3a7a4
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-09-21 16:55:27 -0300

    Update Service Car

--- src/service/impl/car.service.impl.ts
+++ src/service/impl/car.service.impl.ts
@@ -1,27 +1,24 @@
 import Car from "../../models/car.model";
-import CarRepository from "../../repository/impl/car.repository.impl";
+import carRepository from "../../repository/impl/car.repository.impl";
 import { ICarService } from "../car.service";
-import { ICarRepository } from "../../repository/car.repository";
 import { CarCreationAttributes, CarAttributes } from "../../interface/car.interface";
 
 class CarServiceImpl implements ICarService {
-
-    private carRepository: ICarRepository = CarRepository;
     
     async createCar(car: CarCreationAttributes): Promise<Car> {
-        return await this.carRepository.createCar(car);
+        return await carRepository.createCar(car);
     }
     async findAllCars(): Promise<Car[]> {
-        return await this.carRepository.findAllCars();
+        return await carRepository.findAllCars();
     }
     async findByIdCar(id: number): Promise<Car | null> {
-        return await this.carRepository.findByIdCar(id);
+        return await carRepository.findByIdCar(id);
     }
     async updateCar(id: number, car: Partial<CarAttributes>): Promise<Car | null> {
-        return await this.carRepository.updateCar(id, car);
+        return await carRepository.updateCar(id, car);
     }
     async deleteCar(id: number): Promise<void> {
-        return await this.carRepository.deleteCar(id);
+        return await carRepository.deleteCar(id);
     }
 }
 
