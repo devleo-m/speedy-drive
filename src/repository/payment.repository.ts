387fe8636b85274ce6f2c6f92e272c54528ff520
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-09-22 14:42:31 -0300

    Create payment.repository.ts

---
+++ src/repository/payment.repository.ts
@@ -0,0 +1,10 @@
+import Payment from "../models/payment.model";
+import { PaymentAttributes, PaymentCreationAttributes } from "../interface/payment.interface";
+
+export interface IPaymentRepository {
+    createPayment(payment: PaymentCreationAttributes): Promise<Payment>;
+    findAllPayments(): Promise<Payment[]>;
+    findByIdPayment(id: number): Promise<Payment | null>;
+    updatePayment(id: number, payment: Partial<PaymentAttributes>): Promise<Payment | null>;
+    deletePayment(id: number): Promise<void>;
+}
