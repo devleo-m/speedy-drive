a8e1b03136ca8a92f89655d9bdbfff3c9a3fb9f7
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-09-22 13:33:00 -0300

    Create payment.model.ts

---
+++ src/models/payment.model.ts
@@ -0,0 +1,61 @@
+import { DataTypes, Model, Sequelize } from "sequelize";
+import { PaymentAttributes, PaymentCreateAttributes } from "../interface/payment.interface";
+import { sequelize } from "../config/database";
+import Rental from "./rental.model";
+
+class Payment extends Model<PaymentAttributes, PaymentCreateAttributes> implements PaymentAttributes {
+    id!: number;
+    method!: "DEBIT" | "CREDIT" | "PIX" | "MONEY";
+    status!: "PAID" | "UNPAID";
+    paymentDate!: Date;
+    rentalId!: number;
+}
+
+Payment.init({
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+    },
+    method: {
+        type: DataTypes.ENUM('DEBIT','CREDIT', 'PIX', 'MONEY'),
+        allowNull: false,
+        defaultValue: 'MONEY'
+    },
+    status: {
+        type: DataTypes.ENUM('PAID', 'UNPAID'),
+        allowNull: false,
+        defaultValue: 'UNPAID'
+    },
+    paymentDate: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+    },
+    rentalId: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    }
+},{
+    sequelize,
+    modelName: 'Payment',
+    tableName: 'Payments',
+    timestamps: false
+});
+
+Rental.hasOne(Payment, {
+    foreignKey: 'rentalId',
+    sourceKey: 'id',
+    onUpdate: 'CASCADE',
+    onDelete: 'CASCADE',
+})
+
+Payment.belongsTo(Rental, {
+    foreignKey: 'rentalId',
+    targetKey: 'id',
+    onUpdate: 'CASCADE',
+    onDelete: 'CASCADE',
+})
+
+export default Payment;
