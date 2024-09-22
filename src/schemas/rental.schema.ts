9eefa1e5db2e3adfecf5e580978ba67f443bbea3
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-09-22 12:22:40 -0300

    Bug fix – add dates

--- src/schemas/rental.schema.ts
+++ src/schemas/rental.schema.ts
@@ -1,9 +1,8 @@
 import { z } from 'zod';
 
 export const rentalSchema = z.object({
-    startDate: z.date().min(new Date(), { message: "The start date cannot be in the past" }),
-    endDate: z.date().min(new Date(), { message: "The end date cannot be in the past" }),
-    price: z.coerce.number().min(0, { message: "The price must be a positive number" }),
+    startDate: z.coerce.date().min(new Date(), { message: "The start date cannot be in the past" }),
+    endDate: z.coerce.date().min(new Date(), { message: "The end date cannot be in the past" }),
     userId: z.coerce.number().min(1, { message: "The user ID must be a positive number" }),
     carId: z.coerce.number().min(1, { message: "The car ID must be a positive number" }),
 }).refine((data) => data.endDate > data.startDate, {
@@ -12,9 +11,8 @@ export const rentalSchema = z.object({
 });
 
 export const rentalUpdateSchema = z.object({
-    startDate: z.date().min(new Date(), { message: "The start date cannot be in the past" }).optional(),
-    endDate: z.date().min(new Date(), { message: "The end date cannot be in the past" }).optional(),
-    price: z.coerce.number().min(0, { message: "The price must be a positive number" }).optional(),
+    startDate: z.coerce.date().min(new Date(), { message: "The start date cannot be in the past" }).optional(),
+    endDate: z.coerce.date().min(new Date(), { message: "The end date cannot be in the past" }).optional(),
     userId: z.coerce.number().min(1, { message: "The user ID must be a positive number" }).optional(),
     carId: z.coerce.number().min(1, { message: "The car ID must be a positive number" }).optional(),
 }).refine((data) => {
