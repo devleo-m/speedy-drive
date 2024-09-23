913cfd5c89ec0888547627e064cb8b29836fc6cd
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-09-22 21:27:36 -0300

    Update user.schema.ts

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -11,5 +11,5 @@ export const userUpdateSchema = z.object({
     name: z.string().optional(),
     email: z.string().email({ message: "Invalid email format" }).optional(),
     password: z.string().min(6, { message: "Password must be at least 6 characters long" }).optional(),
-    role: z.enum(["ADMIN", "CLIENT"]).optional().default("CLIENT"),
+    role: z.enum(["ADMIN", "CLIENT"]).optional(),
 });
