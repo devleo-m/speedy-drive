6e4ee64bd079d375caf447fef438cc52cce083c3
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-09-22 21:27:23 -0300

    Update car.router.ts

--- src/routes/car.router.ts
+++ src/routes/car.router.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import { CarController } from "../controller/car.controller";
+import authMiddleware from "../middleware/auth.middleware";
+import isAdmin from "../middleware/isAdmin.middleware";
+
 const router = Router();
 
-router.post("/cars", CarController.createCar);
-router.get("/cars", CarController.findAllCars);
-router.get("/cars/:id", CarController.findByIdCar);
-router.put("/cars/:id", CarController.updateCar);
-router.delete("/cars/:id", CarController.deleteCar);
+router.post("/cars", authMiddleware, isAdmin, CarController.createCar);
+router.get("/cars", authMiddleware, isAdmin, CarController.findAllCars);
+router.get("/cars/:id", authMiddleware, isAdmin, CarController.findByIdCar);
+router.put("/cars/:id", authMiddleware, isAdmin, CarController.updateCar);
+router.delete("/cars/:id", authMiddleware, isAdmin, CarController.deleteCar);
 
 export default router;
