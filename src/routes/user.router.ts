26ec3f26d62fd0e6c28626c4b0b2f201757be0f2
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-09-22 21:27:30 -0300

    Update user.router.ts

--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { UserController } from "../controller/user.controller";
+import authMiddleware from "../middleware/auth.middleware";
+import isAdmin from "../middleware/isAdmin.middleware";
 const router = Router();
 
-router.post("/users", UserController.createUser);
-router.get("/users", UserController.findAllUsers);
-router.get("/users/:id", UserController.findUserById);
-router.put("/users/:id", UserController.updateUser);
-router.delete("/users/:id", UserController.deleteUser);
+router.post("/users", authMiddleware, isAdmin, UserController.createUser);
+router.get("/users", authMiddleware, isAdmin, UserController.findAllUsers);
+router.get("/users/:id", authMiddleware, isAdmin, UserController.findUserById);
+router.put("/users/:id", authMiddleware, isAdmin, UserController.updateUser);
+router.delete("/users/:id", authMiddleware, isAdmin, UserController.deleteUser);
 
 export default router;
