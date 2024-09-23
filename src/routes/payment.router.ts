39d9e4ecd9bbe8ad6dec81be0b9dfe6a976883c3
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-09-22 21:27:25 -0300

    Update payment.router.ts

--- src/routes/payment.router.ts
+++ src/routes/payment.router.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { PaymentController } from "../controller/payment.controller";
+import authMiddleware from "../middleware/auth.middleware";
+import isAdmin from "../middleware/isAdmin.middleware";
 const router = Router();
 
-router.post("/payments", PaymentController.createPayment);
-router.get("/payments", PaymentController.listAllPayments);
-router.get("/payments/:id", PaymentController.listPaymentById);
-router.put("/payments/:id", PaymentController.updatePayment);
-router.delete("/payments/:id", PaymentController.deletePayment);
+router.post("/payments", authMiddleware, isAdmin, PaymentController.createPayment);
+router.get("/payments", authMiddleware, isAdmin, PaymentController.listAllPayments);
+router.get("/payments/:id", authMiddleware, isAdmin, PaymentController.listPaymentById);
+router.put("/payments/:id", authMiddleware, isAdmin, PaymentController.updatePayment);
+router.delete("/payments/:id", authMiddleware, isAdmin, PaymentController.deletePayment);
 
 export default router;
