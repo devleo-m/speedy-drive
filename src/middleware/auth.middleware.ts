8e9f86c176d8642e3d41885c8b87731044c72b5a
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-09-25 14:19:15 -0300

    Update auth.middleware.ts

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -6,14 +6,12 @@ const JWT_SECRET = process.env.JWT_SECRET || 'izi-jwt-secret';
 
 function authMiddleware(req: Request, res: Response, next: NextFunction) {
 
-
     const authHeader = req.headers.authorization;
 
     if (!authHeader) {
         return res.status(401).json({ message: `The token is not valid` });
     }
 
-    
     const token = authHeader.split(' ')[1];
 
     if (token === '[free:token]') {
